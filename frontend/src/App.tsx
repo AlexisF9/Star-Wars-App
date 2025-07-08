4c9ab5f3e7785818257d226e68f78c5369f1b5c4
AlexisF9 <alexis.flacher38@gmail.com>
2025-07-08 19:29:12 +0200

    add tanstack query in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,11 @@
-import { useEffect, useState } from "react";
 import "./App.css";
+import { Item } from "./components/item";
 
 function App() {
-  const [data, setData] = useState<any>(null);
-
-  useEffect(() => {
-    test();
-  }, []);
-
-  const test = async () => {
-    const url = "http://localhost:3000/api/category/films/1";
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-
-      const json = await response.json();
-      setData(json);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   return (
     <>
       <h1>Hello</h1>
-      {data && <p>{data.title}</p>}
+      <Item />
     </>
   );
 }
