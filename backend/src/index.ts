16a49d6cc54b5d4fde438f75334ed4f35f8f2b54
AlexisF9 <alexis.flacher38@gmail.com>
2025-07-08 17:11:50 +0200

    add cors in backend + start front and tailwind installation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import searchRoute from './routes/search.route';
 import categoryRoute from './routes/category.route';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// seulement le front React local est autorisé à accèder à l'API
+app.use(cors({
+  origin: 'http://localhost:5173',
+}));
 app.use(express.json());
 app.use('/api/search', searchRoute);
 app.use('/api/category', categoryRoute);
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState<any>(null);
+
+  useEffect(() => {
+    test();
+  }, []);
+
+  const test = async () => {
+    const url = "http://localhost:3000/api/category/films/1";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const json = await response.json();
+      setData(json);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello</h1>
+      {data && <p>{data.title}</p>}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
